96a3b047109b248a59b3a9142c319e5fd2a9f76c
Vinoth Subramanian <157690987+Vino-Siva@users.noreply.github.com>
2024-09-11 13:01:06 +0530

    add recent projects using Aceternity 3d-pin component

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,9 +1,66 @@
+// "use client";
+import { projects } from "@/data";
 import React from "react";
+import { PinContainer } from "./ui/3d-pin";
+import { FaArrowUpRightFromSquare } from "react-icons/fa6";
 
 const RecentProjects = () => {
   return (
-    <div>
-      <h1>RecentProjects</h1>
+    <div className="py-20">
+      <h1 className="heading">
+        A small selection of my{" "}
+        <span className="text-purple">recent projects</span> and{" "}
+        <span className="text-purple">experiments</span>
+      </h1>
+      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
+        {projects.map(({ id, title, des, img, iconLists, link }) => (
+          <div
+            key={id}
+            className="lg:min-h-[32.5rem] sm:h-[41rem] h-[32rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
+          >
+            <PinContainer title={link} href={link}>
+              <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10">
+                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162D]">
+                  <img src="/bg.png" alt="background" aria-hidden="true" />
+                </div>
+                <img src={img} alt={title} className="z-10 absolute bottom-0" />
+              </div>
+              <h1 className=" lg:text-2xl md:text-xl text-base font-bold line-clamp-1">
+                {title}
+              </h1>
+              <p className="lg:text-lg lg:font-extralight font-light text-sm line-clamp-2">
+                {des}
+              </p>
+              <div className="flex items-center justify-between mt-7 mb-3">
+                <div className="flex items-center">
+                  {iconLists.map((icon, index) => (
+                    <div
+                      key={icon}
+                      className="border border-white/[0.2] rounded-full bg-black lg:size-10 size-8 flex justify-center items-center"
+                      style={{
+                        transform: `traslateX(-${5 * index * 2}px)`,
+                      }}
+                    >
+                      <img
+                        src={icon}
+                        alt={icon}
+                        aria-hidden="true"
+                        className="p-2"
+                      />
+                    </div>
+                  ))}
+                </div>
+                <div className="flex justify-center items-center">
+                  <p className="flex lg:text-xl md:text-xs text-sm">
+                    Live Site
+                  </p>
+                  <FaArrowUpRightFromSquare className="ms-3" color="#CBACF9" />
+                </div>
+              </div>
+            </PinContainer>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
